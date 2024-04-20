67a7e7bac1acae26e99b14fbb021db18a8d2241d
BrunoPantaleao92 <bruno_92k@hotmail.com>
2024-04-19 21:41:43 -0300

    Exercicio 06

--- src/br/edu/up/modelos/Venda06.java
+++ src/br/edu/up/modelos/Venda06.java
@@ -1,14 +1,32 @@
 package br.edu.up.modelos;
-public class Venda06 {
-    public double precoCusto;
-    public double percentualAcrescimo;
 
-    public double valorVenda(){
+public class Venda06 {
+    private double precoCusto;
+    private double percentualAcrescimo;
+
+    public double getPrecoCusto() {
+        return precoCusto;
+    }
+
+    public void setPrecoCusto(double precoCusto) {
+        this.precoCusto = precoCusto;
+    }
+
+    public double getPercentualAcrescimo() {
+        return percentualAcrescimo;
+    }
+
+    public void setPercentualAcrescimo(double percentualAcrescimo) {
+        this.percentualAcrescimo = percentualAcrescimo;
+    }
+
+    public double valorVenda() {
         return precoCusto + (precoCusto * percentualAcrescimo);
     }
-    public String toString(){
+
+    public String toString() {
         return "O preço de custo é R$:" + precoCusto + "\n" +
-        "O valor de venda é R$:" + valorVenda();
+                "O valor de venda é R$:" + valorVenda();
     }
-    
+
 }
