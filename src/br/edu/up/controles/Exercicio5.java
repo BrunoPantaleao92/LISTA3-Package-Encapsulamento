6027a0130b66663c8ac1194a6a32a3e4327d0599
BrunoPantaleao92 <bruno_92k@hotmail.com>
2024-04-19 21:38:22 -0300

    Exercicio 05

--- src/br/edu/up/controles/Exercicio5.java
+++ src/br/edu/up/controles/Exercicio5.java
@@ -1,19 +1,22 @@
 package br.edu.up.controles;
+
 import br.edu.up.modelos.Venda;
 import br.edu.up.util.Prompt;
 
 public class Exercicio5 {
-    public static void executar(){
-        /*A Loja Mamão com Açúcar está vendendo seus produtos em 5 (cinco) prestações sem juros.
-        Faça um programa que receba um valor de uma compra e mostre o valor das prestações. */
+    public static void executar() {
+        /*
+         * A Loja Mamão com Açúcar está vendendo seus produtos em 5 (cinco) prestações
+         * sem juros.
+         * Faça um programa que receba um valor de uma compra e mostre o valor das
+         * prestações.
+         */
 
         Venda venda1 = new Venda();
 
-        venda1.totalVenda = Prompt.lerDecimal("Digite o valor total da venda: ");
+        venda1.setTotalVenda(Prompt.lerDecimal("Digite o valor total da venda: "));
 
         Prompt.imprimir(venda1.toString());
-
-
 
     }
 
--- src/br/edu/up/modelos/Venda.java
+++ src/br/edu/up/modelos/Venda.java
@@ -1,17 +1,26 @@
 package br.edu.up.modelos;
+
 public class Venda {
     // propriedades
 
-    public double totalVenda;
+    private double totalVenda;
 
-    //metodo
+    // metodo
 
-    public double prestacoes(){
+    public double prestacoes() {
         return totalVenda / 5;
     }
 
-    public String toString(){
+    public String toString() {
         return "Total R$:" + totalVenda + "\n" +
-        "Valor da prestação em 5x é R$:" + prestacoes();
+                "Valor da prestação em 5x é R$:" + prestacoes();
+    }
+
+    public double getTotalVenda() {
+        return totalVenda;
+    }
+
+    public void setTotalVenda(double totalVenda) {
+        this.totalVenda = totalVenda;
     }
 }
